18eaa9c5a8ddb886bdac324b748bc5edd1d34887
Jong Myoung <djm342@naver.com>
2025-02-24 21:45:50 +0900

    v0.6 day11 assignment

--- day11.py
+++ day11.py
@@ -1,21 +1,16 @@
-# from sklearn.linear_model import LinearRegression
-# import tglearn as tg
-from dolearn import LinearRegression
+# Assignment
+# v0.7) v0.6의 최근접이웃모델과 같이 동작하는 커스텀 클래스를 설계하시오.
+
+from dolearn import KNeighborsRegressor
 import pandas as pd
 import matplotlib.pyplot as plt
 
 ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
-X = ls[["GDP per capita (USD)"]].values
-y = ls[["Life satisfaction"]].values
 
-ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
-plt.axis([23500, 62500, 4, 9])
-plt.show()
+model = KNeighborsRegressor()
+result = model.sort(ls,31721.3)
 
-model = LinearRegression()
-model.fit(X, y)
+print(model.predict(result))
+print(model)
 
-X_new = [[31721.3]]  # ROK 2020
-print(model.predict(X_new))
-# LinearRegression 5.90
 # KNeighborsRegressor 5.70
--- dolearn.py
+++ dolearn.py
@@ -1,30 +1,47 @@
 import numpy as np
+import pandas as pd
 
-class LinearRegression:
-    def __init__(self):
-        self.slope = None # 기울기
-        self.intercept = None # 절편
+
+class KNeighborsRegressor:
+    def __init__(self, n_neighbors = 5): #입력안하면 디폴트 값 5로 설정
+        self.n_neighbors = n_neighbors
+
+
+
+    def sort(self, Df, X):
+        col_2 = Df.iloc[:, [1]].values.tolist() #2번째 세로줄
+        col_3 = Df.iloc[:, [2]].values.tolist() #3번째 세로줄
+        abs_list = [] # 절대값을 받을 리스트
+
+        #절대값을 넣을 for문
+        for i in col_2:
+            abs_list.append(abs(i[0] - X)) #abs 리스트에 추가 완료
+
+        rows, cols = Df.shape # 길이를 알아내기 위함
+
+        new_list = [] #GDP를 절대값으로 차로 바꾼 리스트
+
+        for i in range(0,rows):
+            new_list.append([abs_list[i], col_3[i]])
+
+        new_list.sort(key=lambda x: x[0])
+        return new_list
+
 
     def fit(self, X, y):
-        """
-        learning function
-        :param X: independent variable (2d array format)
-        :param y: dependent variable (2d array format)
-        :return: none
-        """
-        X_mean = np.mean(X)
-        y_mean = np.mean(y)
 
-        denominator = np.sum(pow(X_mean, 2))
-        numerator = np.sum((X - X_mean) * (y - y_mean))
+        pass
 
-        self.slope = numerator / denominator
-        self.intercept = y_mean - (self.slope * X_mean)
+    def predict(self, sorted_list) -> np.ndarray:
 
-    def predict(self, X) -> np.ndarray:
-        """
-        predict value for input
-        :param X: new indepent variable
-        :return: predict value for input (2d array format)
-        """
-        return self.slope * np.array(X) + self.intercept
+        happy = 0
+        print(sorted_list)
+        for i in range(self.n_neighbors):
+            happy = happy + float(sorted_list[i][1][0])
+
+        happy = happy / self.n_neighbors
+
+        return happy
+
+#predict를 사용하면 n_neighbors= k만큼 가까운 수 에서 뽑은 수들을 뽑아 평균을 냄.
+
