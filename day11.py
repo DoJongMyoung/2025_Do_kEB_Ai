7d6e1be5a3b7ace9f856274fdcb7c4297a20c3d1
Jong Myoung <djm342@naver.com>
2025-02-24 11:34:41 +0900

    v0.2 day 11 pandas df

--- day11.py
+++ day11.py
@@ -1,4 +1,13 @@
 import numpy as np
 
-a = np.array([1, 3, -9, -2]) #numpy는 안에 있는 요소들은 같은 타입으로 들어가 있어 빠름 .
-print(a, a. ndim)
+import pandas as pd
+
+df = pd.DataFrame(
+{  "a" : [4, 5, 6],
+        "b" : [7, 8, 9],
+        "c" : [10, 11, 12]},    )
+#index는 행에 대한 index 역할을 함.
+#딕셔너리의 키들은 열에 대한 이름 역할을 함.
+print(df)
+
+#index = [1, 2, 3]를 제거할 경우 0 1 2로 시작함.
