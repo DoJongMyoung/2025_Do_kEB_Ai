192196b8f94057649a2db0741e55cb41343fc328
Jong Myoung <djm342@naver.com>
2025-02-26 18:17:07 +0900

    v1.5 mpg

--- day13.py
+++ day13.py
@@ -1,20 +1,13 @@
-class Pokemon:
-    def __init__(self,name):
-        self.name = name
+import seaborn as sns
 
-    def walks(self,name):
-        print("walk..")
-    def attack(self, target_pokemon):
-        print(f"{self.name} attack {target_pokemon.name} !")
+mpg = sns.load_dataset('mpg')
 
+mpg.info()
 
-class Pikachu(Pokemon):
-    pass
+print(mpg.sample(5).to_string())
 
-class Agumon:
-    def __init__(self,name):
-        self.name = name
+print(mpg[["horsepower"]].tail())
 
-agumon = Agumon('아구몬')
-pikachu = Pikachu('피카츄')
-pikachu.attack(agumon)
+#마력이 높으면 기름을 많이 먹음
+#horsepoewer에 결측치가 6개 존재함
+#해결 방법 1. 6개의 결측치를 다른 값으로 넣음 2. 결측치를 보여주는 6개를 날려버리기.
